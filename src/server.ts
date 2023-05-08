54e827de04cec282bdcb97abf9084f221f8dced2
Pedro Lopes <lopesphls@gmail.com>
2023-05-08 13:03:20 -0300

    chore: add typeorm and postgres

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,20 @@
-import { log } from 'console'
-import express, { Request, Response } from 'express'
 
+import express, { Request, Response, json, urlencoded } from 'express'
+import 'reflect-metadata'
+
+function Server(){
 const app = express()
+
+const port = process.env.PORT || 3000
 
 app.get('/', ((req: Request,res:Response)=> {return res.json('funcionando')}))
 
+app.use(urlencoded({extended:true}))
+app.use(json())
 
-app.listen(3000, ()=>{
-    log('servidor rodando na porta 3000')
+
+app.listen(port, ()=>{
+    console.log(`servidor rodando em http://localhost:${port}`)
 })
+}
+Server()
